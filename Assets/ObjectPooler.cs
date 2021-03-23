60d8de285ada39ef4dd66900faec8c3636b13261
DESKTOP-Aadarsh <aadarshkomera@gmail.com>
2021-03-23 11:04:52 +0530

    touch controls updated

---
+++ Assets/ObjectPooler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    #region singelton class
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> bulletPoolDictionary;
+
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletPoolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            bulletPoolDictionary.Add(pool.tag, objectPool);
+        }
+
+    }
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!bulletPoolDictionary.ContainsKey(tag))
+        {
+            // Debug.LogWarning("pool with tag" + tag + " doesn't exist");
+            return null;
+        }
+        GameObject objectToSpawn = bulletPoolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        bulletPoolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
---
+++ Assets/SawSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawSpawner : MonoBehaviour
+{
+    public float time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  ObjectPooler.Instance.SpawnFromPool("bullet", transform.position, Quaternion.identity);
+    }
+    GameObject temp1;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (time >= 3.0f)
+        {
+            if (temp1 != null)
+                temp1.SetActive(false);
+            Vector3 enemySpawnPosition = new Vector3(1.9f, -3.9f, 0);
+            GameObject temp = ObjectPooler.Instance.SpawnFromPool("Saw", enemySpawnPosition, Quaternion.Euler(0f, 0f, 180f));
+            temp1 = temp;
+            time = 0.0f;
+
+
+        }
+        time += Time.deltaTime;
+
+    }
+}
